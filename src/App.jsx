b0b38e18898020f2dfa0a5b17186ccf3be91387c
Joe <=>
2021-06-17 21:40:01 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+
+function App() {
+  const name = 'Hitomi'
+
+  return (
+    <div className='container'>
+      <Header/>
+    </div>
+  )
+}
+
+
+
+export default App
